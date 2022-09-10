ed7bf6d2fbfff9bcd2b8551f0c290998b5ce7086
rahimedu <edwin.duche82@gmail.com>
2022-09-10 08:35:40 -0400

    message

--- app/assets/javascripts/api_key_form.js
+++ app/assets/javascripts/api_key_form.js
@@ -1,7 +1,3 @@
-$(function() {
-  var enableGemScopeCheckboxes = $("#push_rubygem, #yank_rubygem, #add_owner, #remove_owner");
-  var hiddenRubygemId = "hidden_api_key_rubygem_id";
-  toggleGemSelector();
 
   enableGemScopeCheckboxes.click(function() {
     toggleGemSelector();

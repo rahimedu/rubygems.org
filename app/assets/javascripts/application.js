cf62c3327ab29e5040702ea5af6f3567035ef9fc
rahimedu <edwin.duche82@gmail.com>
2022-09-10 09:09:56 -0400

    initmessage

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,8 +10,6 @@
 //= require github_buttons
 //= require_tree .
 
-function handleClick(event, nav, removeNavExpandedClass, addNavExpandedClass) {
-  var isMobileNavExpanded = nav.popUp.hasClass(nav.expandedClass);
 
   event.preventDefault();
 
